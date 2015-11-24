c0f9bfd7bd3e68f8875a776d483a4b973c1183c6
Roald <Roaldteunissen@gmail.com>
2015-11-24 13:16:01 +0100

    Taps replaced with spaces

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -9,42 +9,42 @@ public class Network : MonoBehaviour
 {
     public Text text;
 
-	void Start()
-	{
-		Application.runInBackground = true;
-		
-		int port = 9876;
-		IPAddress localhost = IPAddress.Parse("127.0.0.1");
-		_server = new TcpListener(localhost, port);
-		_server.Start();
-		_server.BeginAcceptTcpClient(new AsyncCallback(OnAcceptTcpClient), _server);
-	}
-	
-	void Update()
-	{
-		if (_client != null && _client.Available > 0)
-		{
-			string message = FilterDataIntoMessage();
+    void Start()
+    {
+        Application.runInBackground = true;
+
+        int port = 9876;
+        IPAddress localhost = IPAddress.Parse("127.0.0.1");
+        _server = new TcpListener(localhost, port);
+        _server.Start();
+        _server.BeginAcceptTcpClient(new AsyncCallback(OnAcceptTcpClient), _server);
+    }
+
+    void Update()
+    {
+        if (_client != null && _client.Available > 0)
+        {
+            string message = FilterDataIntoMessage();
             if (message != "")
             {
                 text.text = message;
                 Debug.Log(string.Format("Message received:\n{0}", message));
             }
-		}
-	}
-	
-	private string FilterDataIntoMessage()
-	{
-		Debug.Assert(_client != null, "The client is empty.");
-		Debug.Assert(_client.Available > 0, "The client is not available.");
-		
-		NetworkStream stream = null;
+        }
+    }
+
+    private string FilterDataIntoMessage()
+    {
+        Debug.Assert(_client != null, "The client is empty.");
+        Debug.Assert(_client.Available > 0, "The client is not available.");
+
+        NetworkStream stream = null;
 
         stream = _client.GetStream();
-			
-		while (stream.DataAvailable) 
-		{
-			int i = stream.ReadByte();
+
+        while (stream.DataAvailable) 
+        {
+            int i = stream.ReadByte();
             if (i == 10)
             {
                 string msg = message.ToString();
@@ -56,19 +56,19 @@ public class Network : MonoBehaviour
             {
                 message.Append((char) i);
             }
-		}
+    }
 
         return String.Empty;
-	}
-	
-	private void OnAcceptTcpClient(IAsyncResult result)
-	{
-		TcpListener _server = (TcpListener) result.AsyncState;
-		_client = _server.EndAcceptTcpClient(result);
-		Debug.Log("Client connected.");
-	}
+    }
+
+    private void OnAcceptTcpClient(IAsyncResult result)
+    {
+        TcpListener _server = (TcpListener) result.AsyncState;
+        _client = _server.EndAcceptTcpClient(result);
+        Debug.Log("Client connected.");
+    }
 
     private TcpListener _server;
-	private TcpClient _client;
+    private TcpClient _client;
     private StringBuilder message = new StringBuilder();
 }
